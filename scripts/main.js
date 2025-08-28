f010854d073bf22ac920b4fd61e9ffa5ae782ba2
Eric Petersen <ericpetersensa@gmail.com>
2025-08-28 11:22:46 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,26 @@
 console.log("Lazy DM Test module loading…");
 
-class TestApp extends ApplicationV2 {
-  static DEFAULT_OPTIONS = {
-    ...super.DEFAULT_OPTIONS,
-    id: "lazy-dm-test-app",
-    title: game.i18n.localize("LAZYDM.Title"),
-    template: "modules/lazy-dm-module/templates/window.hbs",
-    width: 400,
-    height: 300
-  };
-}
-
 Hooks.once("init", () => {
   console.log("Lazy DM Test init hook fired");
+
+  class TestApp extends ApplicationV2 {
+    static DEFAULT_OPTIONS = {
+      ...super.DEFAULT_OPTIONS,
+      id: "lazy-dm-test-app",
+      title: game.i18n.localize("LAZYDM.Title"),
+      template: "modules/lazy-prep-app/templates/window.hbs",
+      width: 400,
+      height: 300
+    };
+  }
+
+  // Register global API
   game.lazyDM = {
     open: () => new TestApp().render(true)
   };
 });
 
+// Scene controls launch button
 Hooks.on("getSceneControlButtons", controls => {
   controls.push({
     name: "lazy-dm",
